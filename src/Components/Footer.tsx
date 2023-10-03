c5a39b354ff21006dc14631deadf6d7bafb1147c
Matthew Creek <matthew.creek27@gmail.com>
2023-10-03 15:59:08 -0500

    small styling change to footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-        <div className="w-full bg-[#5f0003] flex flex-col justify-center items-center pb-2">
+        <div className="w-full bg-[#5f0003] flex flex-col justify-center items-center py-2">
             <p className="text-gray-400 text-center font-thin">Copyright Matthew Creek 2023</p>
         </div>
     )
